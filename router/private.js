e80c044bcedbe3c777f20680de8e57e8e7c2eb04
youxam <zhang.haoling@outlook.com>
2021-02-05 16:16:52 +0800

    基础文章系统

--- router/private.js
+++ router/private.js
@@ -1,32 +1,70 @@
 const Router = require('koa-router')
-const jwt = require('koa-jwt')
-const jsonwebtoken = require('jsonwebtoken')
-
 const app = new Router()
+const mongodb = require('mongodb')
+const ObjectId = mongodb.ObjectId
+
+const tools = require('../lib/tools')
 
 // eslint-disable-next-line no-unused-vars
-module.exports = function(koa, config, db) {
+module.exports = function (koa, config, db) {
     // 未登录时返回错误信息
-    app.use((ctx, next) => {
-        return next().catch((err) => {
-            if (err.status == 401) {
-                ctx.status = 401
-                ctx.json({ code: 401, msg: 'Unauthorized'})
-            } else {
-                throw err
-            }
-        })
-    })
-    // 检测是否登录 & 解码 token
-    app.use(jwt({ secret: config.secret }))
     app.use(async (ctx, next) => {
-        ctx.state.user = jsonwebtoken.decode(ctx.request.headers.authorization.split(' ')[1])
+        if (ctx.state.user) await next()
+        else ctx.json({ code: 401, msg: '未登录' })
+    })
+
+    // 获取用户信息
+    app.get('/api/userinfo', async ctx => {
+        const user = await db.collection('users').findOne({ username: ctx.state.user.username }, { projection: { password: 0 } })
+        ctx.json({ code: 200, data: user })
+    })
+
+    // 新建文章
+    // TODO 将编辑记录插入文章对象中
+    // TODO 将编辑记录插入用户对象中
+    app.post('/api/article/', async ctx => {
+        const title = ctx.request.body.title
+        const content = ctx.request.body.content
+        const author = ctx.state.user.username
+        if (!title || !content || !author) return ctx.json({ code: 400, msg: '参数不足' })
+        const article = await db.collection('articles').insertOne({ title, content, visibility: 1 })
+        ctx.json({ code: 200, article: article.ops })
+    })
+
+    // 获取用户权限
+    app.use(async (ctx, next) => {
+        ctx.state.permission = await tools.get_permission(ctx, db)
         await next()
     })
 
+    // 修改文章内容: 可见性, 标题和内容
+    // TODO 修改标题和内容 - 历史记录
+    // TODO 将编辑记录插入文章对象中
+    // TODO 将编辑记录插入用户对象中
+    app.post('/api/article/:id', async ctx => {
+        const article = await db.collection('articles').findOne({ _id: ObjectId(ctx.params.id) })
+        if (!article || article.visibility > ctx.state.permission) return ctx.json({ code: 404, msg: '找不到此文章'})
+        const update = { $set: {} }
+        if (ctx.request.body.visibility) {
+            if (ctx.request.body.visibility <= ctx.state.permission) update.$set.visibility = parseInt(ctx.request.body.visibility)
+            else return ctx.json({ code: 400, msg: '权限不足' })
+        }
+        if (Object.keys(update.$set).length) {
+            await db.collection('articles').updateOne({ _id: ObjectId(ctx.params.id) }, update)
+            ctx.json({ code: 200, msg: '修改成功' })
+        } else ctx.json({ code: 200, msg: '没有修改' })
+    })
 
-    app.get('/api/userinfo', async ctx => {
-        ctx.json({ code: 200, data: ctx.state.user })
+    // 检查普通管理员权限
+    app.use(async (ctx, next) => {
+        if (ctx.state.permission < 2) return ctx.json({ code: 401, msg: '权限不足' })
+        await next()
+    })
+
+    // 检查超级管理员权限
+    app.use(async (ctx, next) => {
+        if (ctx.state.permission < 3) return ctx.json({ code: 401, msg: '权限不足' })
+        await next()
     })
 
     koa.use(app.routes())
--- router/public.js
+++ router/public.js
@@ -1,18 +1,55 @@
 const Router = require('koa-router')
 const jwt = require('jsonwebtoken')
 const app = new Router()
+const mongodb = require('mongodb')
+const ObjectId = mongodb.ObjectId
+
+const tools = require('../lib/tools')
+
 
 module.exports = function (koa, config, db) {
+
+    // 获取文章列表
+    app.get('/api/articles', async ctx => {
+        let size = parseInt(ctx.query.size) || 20, page = parseInt(ctx.query.page) || 1
+        size = size < 1 ? 1 : size > 50 ? 50 : size
+        page = page < 1 ? 1 : page
+        const args = ctx.query.search ? {
+            $and: [
+                {
+                    visibility: { $lte: await tools.get_permission(ctx, db) }
+                },
+                {
+                    $or: [
+                        { title: { $regex: ctx.query.search, $options: '$i'} },
+                        { content: { $regex: ctx.query.search, $options: '$i'} },
+                    ]
+                }
+            ]
+        } : { visibility: { $lte: await tools.get_permission(ctx, db) } }
+        const query = db.collection('articles').find(args)
+        const articles = await query.skip((page - 1) * size).limit(size).toArray()
+        const total = await query.count()
+        ctx.json({ code: 200, articles, total })
+    })
+
+    // 获取文章
+    app.get('/api/article/:id', async ctx => {
+        const article = await db.collection('articles').findOne({ _id: ObjectId(ctx.params.id), visibility: { $lte: await tools.get_permission(ctx, db) } })
+        if (article) ctx.json({ code: 200, article })
+        else ctx.json({ code: 404, msg: 'Not Found' })
+    })
+
 
     // 登录
     app.post('/api/login', async ctx => {
         const username = ctx.request.body.username
         const password = ctx.request.body.password
-        if (!username || !password) return ctx.json({ code: 400, msg: '参数不足'})
-        const person = await db.collection('users').findOne({ username: username })
+        if (!username || !password) return ctx.json({ code: 400, msg: '参数不足' })
+        const person = await db.collection('users').findOne({ username })
         if (person && person.password === password) {
-            const token = jwt.sign({ username: username, email: person.email }, config.secret, { expiresIn:  config.expires })
-            ctx.json({ code: 200, msg: '登录成功', token: token })
+            const token = jwt.sign({ username }, config.secret, { expiresIn: config.expires })
+            ctx.json({ code: 200, msg: '登录成功', token })
         } else
             ctx.json({ code: 401, msg: '登录失败' })
     })
@@ -22,14 +59,14 @@ module.exports = function (koa, config, db) {
         const username = ctx.request.body.username
         const password = ctx.request.body.password
         const email = ctx.request.body.email
-        if (!username || !password || !email) return ctx.json({ code: 400, msg: '参数不足'})
-        const person = await db.collection('users').findOne({ username: username })
+        if (!username || !password || !email) return ctx.json({ code: 400, msg: '参数不足' })
+        const person = await db.collection('users').findOne({ username })
         if (person) ctx.json({ code: 400, msg: '用户名已被使用' })
         else {
-            await db.collection('users').insertOne({ username: username, password: password, email: email })
+            await db.collection('users').insertOne({ username, password, email, permission: 1 })
             // 注册之后直接返回 token
-            const token = jwt.sign({ username: username, email: email }, config.secret, { expiresIn:  config.expires })
-            ctx.json({ code: 200, msg: '注册成功', token: token })
+            const token = jwt.sign({ username }, config.secret, { expiresIn: config.expires })
+            ctx.json({ code: 200, msg: '注册成功', token })
         }
     })
 
